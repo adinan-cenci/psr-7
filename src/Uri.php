81cecada8d503351a8ddd4fd5fb015d035643703
Adinan Cenci <adinan@zoocha.com>
2022-10-12 20:13:55 -0300

    added Uri::isStandardPort()

--- src/Uri.php
+++ src/Uri.php
@@ -7,11 +7,26 @@ class Uri implements UriInterface
 {
     protected $scheme = '';
 
+    protected static $standardPorts = [
+        'http'  => 80,
+        'https' => 443,
+        'ftp'   => 21,
+        'smtp'  => 587,
+        'imap'  => 993,
+    ];
+
     public function getScheme() 
     {
         $scheme = (string) $this->scheme;
         $scheme = trim($scheme, ':');
         $scheme = strtolower($scheme);
         return $scheme;
+    }
+
+    public static function isStandardPort(int $port, string $scheme) : bool
+    {
+        return isset(self::$standardPorts[$scheme])
+            ? self::$standardPorts[$scheme] == $port
+            : false;
     }
 }
