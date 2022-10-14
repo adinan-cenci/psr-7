78393eef6be489ae1f04a74bb7b8ab3760f513ad
Adinan Cenci <adinancenci@protonmail.com>
2022-10-14 10:40:14 -0300

    added unit-tests

---
+++ tests/UriTest.php
@@ -0,0 +1,13 @@
+<?php 
+namespace AdinanCenci\Psr7\Tests;
+
+use AdinanCenci\Psr7\Uri;
+use Http\Psr7Test\UriIntegrationTest;
+
+class UriTest extends UriIntegrationTest
+{
+    public function createUri($uri) 
+    {
+        return Uri::parseString($uri);
+    }
+}
