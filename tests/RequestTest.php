7eee43239f2b7b918d8c316f2931362c43227a77
Adinan Cenci <adinancenci@protonmail.com>
2025-06-11 00:44:38 +0100

    issue-17: added unit test

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -4,11 +4,20 @@ namespace AdinanCenci\Psr7\Tests;
 
 use AdinanCenci\Psr7\Request;
 use Http\Psr7Test\RequestIntegrationTest;
+use Psr\Http\Message\StreamInterface;
 
 class RequestTest extends RequestIntegrationTest
 {
     public function createSubject()
     {
         return new Request();
+    }
+
+    public function testNewRequestWithEmptyBody()
+    {
+        $request = new Request();
+        $body = $request->getBody();
+
+        $this->assertInstanceOf(StreamInterface::class, $body);
     }
 }
