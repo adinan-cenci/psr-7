d0c9ebc090e1c6873eb53b79d4fe1f155be9b560
Adinan Cenci <adinancenci@protonmail.com>
2026-03-16 21:31:22 +0000

    issue-19: added tests for the cookie features

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -11,4 +11,37 @@ class ResponseTest extends ResponseIntegrationTest
     {
         return new Response();
     }
+
+    public function testMinimalCookieHeader()
+    {
+        $response = new Response();
+        $response = $response->withAddedCookie('cookie_name', 'cookie value');
+
+        $this->assertEquals('cookie_name=cookie value', $response->getHeaders()['Set-Cookie'][0]);
+    }
+
+    public function testCompleteCookieHeader()
+    {
+        $response = new Response();
+
+        $now = time();
+        $twentyFourHours = 60 * 60 * 24;
+
+        $response = $response->withAddedCookie(
+            'cookie_name',
+            'cookie value',
+            $twentyFourHours,
+            $now + $twentyFourHours,
+            '/',
+            'my-domain.com',
+            true,
+            true,
+            'Strict'
+        );
+
+        $this->assertEquals(
+            'cookie_name=cookie value; MaxAge=86400; Expires=' . date('D, d M Y H:i:s', $now + $twentyFourHours) . ' GMT; Domain=my-domain.com; Path=/; SameSite=Strict; Secure; HttpOnly',
+            $response->getHeaders()['Set-Cookie'][0]
+        );
+    }
 }
